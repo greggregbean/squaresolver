b938c13c427c9c0d5bb37489a049c38383d0216d
greggregbean <90098955+greggregbean@users.noreply.github.com>
2021-09-04 17:30:46 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+#include "squaresolver.h"
+#include "testing.h"
+
+static const double EPS = 0.000001;
+
+double Discrim(double a, double b, double c);
+int IsEqual(double val1, double val2);
+int SquareSolver(double a, double b, double c, double *x1, double *x2);
+void PrintRoots(double x1, double x2, int numRoots);
+int Testing();
+
+int main(){
+
+    printf("This program was built by Ivan Zinin.\n\n");
+
+    printf("The autotesting started.\n");
+    if (!Testing()){
+        return 1;
+    };
+    printf("The autotesting completed.\n\n");
+
+    double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
+    printf ("Please, enter the coefficients of the equation:\n\n");
+    scanf("%lg %lg %lg", &a, &b, &c);
+
+    printf ("\n");
+
+    int numRoots = SquareSolver(a, b, c, &x1, &x2);
+    if (numRoots == CountOfRoots :: ERROR) printf("ERROR\n");
+    PrintRoots(x1, x2, numRoots);
+
+    return 0;
+}
+
+
