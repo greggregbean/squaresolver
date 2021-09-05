2b99711ed56f11c130de904a0eed49f7fdd15989
greggregbean <90098955+greggregbean@users.noreply.github.com>
2021-09-05 20:03:05 +0400

    Update squaresolver.cpp

--- squaresolver.cpp
+++ squaresolver.cpp
@@ -24,41 +24,41 @@ int SquareSolver(double a, double b, double c, double *x1, double *x2)
 
    if (!isfinite(a)) {
         printf("a is not double\n");
-        return CountOfRoots :: ERROR;}
+        return ERROR;}
     if (!isfinite(b)) {
         printf("b is not double\n");
-        return CountOfRoots :: ERROR;}
+        return ERROR;}
     if (!isfinite(c)) {
         printf("c is not double\n");
-        return CountOfRoots :: ERROR;}
+        return ERROR;}
     if (&x1 == NULL){
         printf("x1 has no adress\n");
-        return CountOfRoots :: ERROR;}
+        return ERROR;}
     if (&x2 == NULL){
         printf("x2 has no adress\n");
-        return CountOfRoots :: ERROR;
+        return ERROR;
     }
 
     if (IsEqual(a, 0)){
         if (IsEqual(b, 0)){
-            if (IsEqual(c, 0)) return CountOfRoots :: INFROOTS;
-            else return CountOfRoots :: ZEROROOTS;
+            if (IsEqual(c, 0)) return INFROOTS;
+            else return ZEROROOTS;
         }
         else {
             *x1 = -c/b;
-            return CountOfRoots :: ONEROOT;
+            return ONEROOT;
         }
     }
     else{
         if (IsEqual(c, 0)) {
            if (IsEqual(b, 0)){
                 *x1 = 0;
-                return CountOfRoots :: ONEROOT;
+                return ONEROOT;
            }
            else {
                 *x1 = 0;
                 *x2 = -b/a;
-                return CountOfRoots :: TWOROOTS;
+                return TWOROOTS;
            }
         }
         else {
@@ -66,21 +66,21 @@ int SquareSolver(double a, double b, double c, double *x1, double *x2)
                 if (a*c < 0){
                     *x1 = sqrt(-c/a);
                     *x2 = -sqrt(-c/a);
-                    return CountOfRoots :: TWOROOTS;
+                    return TWOROOTS;
                 }
-                else return CountOfRoots :: ZEROROOTS;
+                else return ZEROROOTS;
             }
             else {
                 if (Discrim(a,b,c) > 0){
                     *x1 = (-b + sqrt(Discrim(a,b,c))) / (2 * a);
                     *x2 = (-b - sqrt(Discrim(a,b,c))) / (2 * a);
-                    return CountOfRoots :: TWOROOTS;
+                    return TWOROOTS;
                 }
                 else if (IsEqual(Discrim(a,b,c), 0)){ // elif
                         *x1 = (-b) / (2 * a);
-                        return CountOfRoots :: ONEROOT;
+                        return ONEROOT;
                 }
-                else return CountOfRoots :: ZEROROOTS;
+                else return ZEROROOTS;
             }
         }
     }
@@ -91,14 +91,15 @@ void PrintRoots(double x1, double x2, int numRoots)
 
     switch(numRoots){
 
-        case CountOfRoots :: ZEROROOTS: printf("This equation has no solve.\n");
+        case ZEROROOTS: printf("This equation has no solve.\n");
                         break;
-        case CountOfRoots :: ONEROOT: printf("This equation has one solve: %lg.\n", x1);
+        case ONEROOT: printf("This equation has one solve: %lg.\n", x1);
                         break;
-        case CountOfRoots :: TWOROOTS: printf("This equation has two solves: %lg and %lg.\n", x1, x2);
+        case TWOROOTS: printf("This equation has two solves: %lg and %lg.\n", x1, x2);
                         break;
-        case CountOfRoots :: INFROOTS: printf("This equation has an infinite set of solves.\n");
+        case INFROOTS: printf("This equation has an infinite set of solves.\n");
                         break;
     }
 
 }
+
